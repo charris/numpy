f9036d24c561e921e045511ec4c3bbb138a61881
Stephan Hoyer <shoyer@google.com>
2017-04-11 17:47:10 -0700

    WIP: NDArrayOperatorsMixin

---
+++ numpy/lib/mixins.py
@@ -0,0 +1,101 @@
+"""
+Mixin classes for writing custom array types that don't inherit from ndarray.
+"""
+from __future__ import division, absolute_import, print_function
+
+import operator
+
+import numpy as np
+
+__all__ = ['NDArrayOperatorsMixin']
+
+
+def _binary_method(ufunc):
+    def func(self, other):
+        try:
+            if other.__array_ufunc__ is None:
+                return NotImplemented
+        except AttributeError:
+            pass
+        return self.__array_ufunc__(ufunc, '__call__', self, other)
+    return func
+
+
+def _reflected_binary_method(ufunc):
+    def func(self, other):
+        try:
+            if other.__array_ufunc__ is None:
+                return NotImplemented
+        except AttributeError:
+            pass
+        return self.__array_ufunc__(ufunc, '__call__', other, self)
+    return func
+
+
+def _inplace_binary_method(ufunc):
+    def func(self, other):
+        result = self.__array_ufunc__(
+            ufunc, '__call__', self, other, out=(self,))
+        if result is NotImplemented:
+            raise TypeError('unsupported operand types for in-place '
+                            'arithmetic: %s and %s'
+                            % (type(self).__name__, type(other).__name__))
+        return result
+    return func
+
+
+def _numeric_methods(ufunc):
+    return (_binary_method(ufunc),
+            _reflected_binary_method(ufunc),
+            _inplace_binary_method(ufunc))
+
+
+def _unary_method(ufunc):
+    def func(self):
+        return self.__array_ufunc__(ufunc, '__call__', self)
+    return func
+
+
+class NDArrayOperatorsMixin(object):
+    """Implements all special methods using __array_ufunc__.
+
+    Caveats: the rarely used `divmod` (``__divmod__``) and unary `+`
+    (``__pos__``) operators do not have corresponding ufuncs. Hence this mixin
+    passes the builtin functions ``divmod`` and ``operator.pos`` to
+    ``__array_ufunc__`` instead of true ufuncs. If you inherit from this mixin,
+    your implementation needs to be able to handle this or you should override
+    these methods.
+    """
+
+    # comparisons don't have reflected and in-place versions
+    __lt__ = _binary_method(np.less)
+    __le__ = _binary_method(np.less_equal)
+    __eq__ = _binary_method(np.equal)
+    __ne__ = _binary_method(np.not_equal)
+    __gt__ = _binary_method(np.greater)
+    __ge__ = _binary_method(np.greater_equal)
+
+    # numeric methods
+    __add__, __radd__, __iadd__ = _numeric_methods(np.add)
+    __sub__, __rsub__, __isub__ = _numeric_methods(np.subtract)
+    __mul__, __rmul__, __imul__ = _numeric_methods(np.multiply)
+    __matmul__, __rmatmul__, __imatmul__ = _numeric_methods(np.matmul)
+    __div__, __rdiv__, __idiv__ = _numeric_methods(np.divide)  # Python 2 only
+    __truediv__, __rtruediv__, __itruediv__ = _numeric_methods(np.true_divide)
+    __floordiv__, __rfloordiv__, __ifloordiv__ = _numeric_methods(
+        np.floor_divide)
+    __mod__, __rmod__, __imod__ = _numeric_methods(np.mod)
+    __divmod__, __rdivmod__, __idivmod__ = _numeric_methods(divmod)
+    # TODO: handle the optional third argument for __pow__?
+    __pow__, __rpow__, __ipow__ = _numeric_methods(np.power)
+    __lshift__, __rlshift__, __ilshift__ = _numeric_methods(np.left_shift)
+    __rshift__, __rrshift__, __irshift__ = _numeric_methods(np.right_shift)
+    __and__, __rand__, __iand__ = _numeric_methods(np.logical_and)
+    __xor__, __rxor__, __ixor__ = _numeric_methods(np.logical_xor)
+    __or__, __ror__, __ior__ = _numeric_methods(np.logical_or)
+
+    # unary methods
+    __neg__ = _unary_method(np.negative)
+    __pos__ = _unary_method(operator.pos)
+    __abs__ = _unary_method(np.absolute)
+    __invert__ = _unary_method(np.invert)
---
+++ numpy/lib/tests/test_mixins.py
@@ -0,0 +1,102 @@
+from __future__ import division, absolute_import, print_function
+
+import numbers
+
+import numpy as np
+from numpy.testing import TestCase, run_module_suite, assert_, assert_equal
+
+
+class ArrayLike(np.NDArrayOperatorsMixin):
+    """An array-like class that wraps NumPy arrays.
+
+    Example usage:
+        >>> x = ArrayLike([1, 2, 3])
+        >>> x - 1
+        ArrayLike(array([0, 1, 2]))
+        >>> 1 - x
+        ArrayLike(array([ 0, -1, -2]))
+        >>> np.arange(3) - x
+        ArrayLike(array([-1, -1, -1]))
+        >>> x - np.arange(3)
+        ArrayLike(array([1, 1, 1]))
+    """
+
+    def __init__(self, value):
+        self.value = np.asarray(value)
+
+    # __array_priority__ = 1000  # for legacy reasons
+
+    _handled_types = (np.ndarray, numbers.Number)
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        # ArrayLike implements arithmetic and ufuncs by deferring to the
+        # wrapped array
+        out = kwargs.get('out', ())
+        for x in inputs + out:
+            # handle _handled_types and superclass instances
+            if not (isinstance(x, self._handled_types) or
+                    isinstance(self, type(x))):
+                return NotImplemented
+
+        inputs = tuple(x.value if isinstance(self, type(x)) else x
+                       for x in inputs)
+        if out:
+            kwargs['out'] = tuple(x.value if isinstance(self, type(x)) else x
+                                  for x in out)
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+        if isinstance(result, tuple):
+            return tuple(type(self)(x) for x in result)
+        else:
+            return type(self)(result)
+
+    def __repr__(self):
+        return '%s(%r)' % (type(self).__name__, self.value)
+
+
+class TestNDArrayOperatorsMixin(TestCase):
+
+    def test_array_like(self):
+
+        x = ArrayLike(1)
+        assert_(isinstance(x + 1, ArrayLike))
+        assert_equal((x + 1).value, 2)
+
+    def test_opt_out(self):
+
+        class OptOut(object):
+            """Object that opts out of __array_ufunc__."""
+            __array_ufunc__ = None
+
+            def __add__(self, other):
+                return self
+
+            def __radd__(self, other):
+                return self
+
+        x = ArrayLike(1)
+        o = OptOut()
+        assert_(isinstance(x + o), OptOut)
+        assert_(isinstance(o + x), OptOut)
+
+    def test_subclass(self):
+
+        class SubArrayLike(ArrayLike):
+            """Should take precedence over ArrayLike."""
+
+        # TODO
+
+    def test_ndarray(self):
+        pass
+
+    def test_ndarray_subclass(self):
+        pass
+
+    def test_out(self):
+        pass
+
+    def test_all_operators(self):
+        pass
+
+
+if __name__ == "__main__":
+    run_module_suite()
